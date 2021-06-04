1cc1dff27df5235f3a0530bb976e9eaacb5e3940
leandroestrella <alexisleandro@gmail.com>
2021-06-04 19:28:12 +0200

    main commit

---
+++ script.js
@@ -0,0 +1,63 @@
+//title change
+function mouseOverTitle() {
+  document.getElementById("titolo").src = "img/title-on.png";
+}
+function mouseOutTitle() {
+  document.getElementById("titolo").src = "img/title.png";
+}
+
+//face change
+function mouseOverFace() {
+  document.getElementById("face").src = "img/scream-final.png";
+}
+function mouseOutFace() {
+  document.getElementById("face").src = "img/calm-final.png";
+}
+
+//calm change
+function mouseOverCalm() {
+  document.getElementById("calmbutton").src = "img/calm-on.png";
+}
+function mouseOutCalm() {
+  document.getElementById("calmbutton").src = "img/calm-off.png";
+}
+
+//normal change
+function mouseOverNormal() {
+  document.getElementById("normalbutton").src = "img/normal-on.png";
+}
+function mouseOutNormal() {
+  document.getElementById("normalbutton").src = "img/normal-off.png";
+}
+
+//strong change
+function mouseOverStrong() {
+  document.getElementById("strongbutton").src = "img/strong-on.png";
+}
+function mouseOutStrong() {
+  document.getElementById("strongbutton").src = "img/strong-off.png";
+}
+
+//again change
+function mouseOverAgain() {
+  document.getElementById("againbutton").src = "img/again-on.png";
+}
+function mouseOutAgain() {
+  document.getElementById("againbutton").src = "img/again-off.png";
+}
+
+//stop change
+function mouseOverStop() {
+  document.getElementById("stopbutton").src = "img/stop-on.png";
+}
+function mouseOutStop() {
+  document.getElementById("stopbutton").src = "img/stop-off.png";
+}
+
+//more change
+function mouseOverMore() {
+  document.getElementById("morebutton").src = "img/more-on.png";
+}
+function mouseOutMore() {
+  document.getElementById("morebutton").src = "img/more-off.png";
+}
